7da29458a9daeeded0b0f29212ce960011b828b2
Kelsey Low <kklowlow@gmail.com>
2019-05-14 03:20:19 +0000

    updating app controller

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,30 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "clb-coll"
+  end
+
+  get "/" do
+    if logged_in?
+      erb :'/climbs'
+    else
+      erb :index
+    end
+  end
+
+  helpers do
+    def current_user
+      User.find_by(id: session[:user_id])
+    end
+
+    def logged_in?
+      !!current_user
+    end
+  end
+
+end
