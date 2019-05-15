53cad1a6ddaf4aed226ba88868e445ecab00ee01
Kelsey Low <kklowlow@gmail.com>
2019-05-15 01:54:22 +0000

    updating climbs.erb, login.erb, and signup,erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,48 @@
 class UsersController < ApplicationController
+  
+  get '/signup' do
+    if !logged_in?
+      erb :'users/signup'
+    else
+      erb :'climbs/climbs'
+    end
+  end
 
+  post '/signup' do
+    if params[:username].empty? || params[:email].empty? || params[:password].empty?
+      redirect '/signup'
+    else
+      @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+      session[:user_id] = @user.id
+      redirect '/climbs'
+    end
+  end
+
+  get '/login' do
+    if !logged_in?
+      erb :'/users/login'
+    else
+      erb :'climbs/climbs'
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(params[:id])
+    if @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect '/climbs'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.clear
+      redirect '/'
+    else
+      redirect '/'
+    end
+  end
+  
 end
