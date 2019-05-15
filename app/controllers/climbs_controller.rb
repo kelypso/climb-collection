e857ca8ef390d5aaeefbaf16ecc051063db6b089
Kelsey Low <kklowlow@gmail.com>
2019-05-15 23:35:12 +0000

    update climbs_controller.rb

--- app/controllers/climbs_controller.rb
+++ app/controllers/climbs_controller.rb
@@ -1,3 +1,60 @@
 class ClimbsController < ApplicationController
+  
+  get '/climbs' do
+    if logged_in?
+      erb :'/climbs/climbs'
+    else
+      redirect '/login'
+    end
+  end
 
+  get '/climbs/new' do
+    if logged_in?
+      erb :'climbs/new'
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/climbs' do
+    if params[:name].empty? || params[:type].empty? || params[:grade].empty? || params[:location].empty? || params[:status].empty? ||
+      !logged_in?
+      redirect '/climbs/new'
+    else
+      @climb = Climb.create(name: params[:name], type: params[:type], grade: params[:grade], location: params[:location], status: params[:status], notes: params[:notes])
+      @climb.user = User.find_by(params[:id])
+      @climb.save
+      redirect "/climbs/#{@climb.id}"
+    end
+  end
+
+  get '/climbs/:id' do
+    if logged_in?
+      @climb = Climb.find_by(params[:id])
+      erb :'/climbs/show'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/climbs/:id/edit' do
+    if logged_in?
+      @climb = Climb.find_by(params[:id])
+      erb :'/climbs/edit'
+    else
+      redirect '/login'
+    end
+  end
+
+  patch '/climbs/:id' do
+    @climb = Climb.find_by(params[:id])
+    if params[:name].empty? || params[:type].empty? || params[:grade].empty? || params[:location].empty? || params[:status].empty?
+      redirect "/climbs/#{@climb.id}/edit"
+    else
+      @climb.update(name: params[:name], type: params[:type], grade: params[:grade], location: params[:location], status: params[:status], notes: params[:notes])
+      @climb.save
+      redirect "/climbs/#{@climb.id}"
+    end
+  end
+  
 end
