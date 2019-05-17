d84470fa9ef0fbf9472bb1c8012da44a09e486a1
Kelsey Low <kklowlow@gmail.com>
2019-05-17 23:33:05 +0000

    updating slugifiable

--- app/models/climb.rb
+++ app/models/climb.rb
@@ -1,3 +1,8 @@
 class Climb < ActiveRecord::Base
+  include Slugifiable::InstanceMethods
+  extend Slugifiable::ClassMethods
+  
   belongs_to :user
+  
+  validates :name, uniqueness: { case_sensitive: false }
 end
