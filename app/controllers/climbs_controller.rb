bfd85e639a0ee13c85222b51a01d93d86638ca7c
Kelsey Low <kklowlow@gmail.com>
2019-05-18 02:10:22 +0000

    adjusting spacing

--- app/controllers/climbs_controller.rb
+++ app/controllers/climbs_controller.rb
@@ -18,47 +18,50 @@ class ClimbsController < ApplicationController
     end
   end
 
- post '/climbs' do
-   if params[:name].empty? || params[:location].empty? || params[:status].empty?
-     redirect '/failure'
-   else
-     @climb = Climb.create(name: params[:name], category: params[:category], grade: params[:grade], location: params[:location], status: params[:status], notes: params[:notes])
-     @climb.user = User.find_by(params[:id])
-     @climb.save
-     redirect "/climbs/#{@climb.id}"
-   end
- end
+  post '/climbs' do
+    if params[:name].empty? || params[:location].empty? || params[:status].empty?
+      redirect '/failure'
+    else
+      @climb = Climb.create(name: params[:name], category: params[:category], grade: params[:grade], location: params[:location], status: params[:status], notes: params[:notes])
+      @climb.user = User.find_by(params[:id])
+      @climb.save
+      redirect "/climbs/#{@climb.id}"
+    end
+  end
 
- get '/climbs/:id' do
-   if logged_in?
-     @climb = Climb.find_by(params[:id])
-     erb :'/climbs/show'
-   else
-     redirect '/login'
-   end
- end
+  get '/climbs/:id' do
+    if logged_in?
+      @user = current_user
+      @climb = Climb.find_by(params[:id])
+      erb :'/climbs/show'
+    else
+      redirect '/login'
+    end
+  end
 
- get '/climbs/:id/edit' do
-   if logged_in?
-     @climb = Climb.find_by(params[:id])
-     erb :'/climbs/edit'
-   else
-     redirect '/login'
-   end
- end
+  get '/climbs/:id/edit' do
+    @user = current_user
+    @climbs = @user.climbs
+    if logged_in?
+      @climb = Climb.find_by(params[:id])
+      erb :'/climbs/edit'
+    else
+      redirect '/login'
+    end
+  end
 
- patch '/climbs/:id' do
-   @climb = Climb.find_by(params[:id])
-   if params[:name].empty? || params[:location].empty? || params[:status].empty?
-     redirect "/climbs/#{@climb.id}/edit"
-   else
-     @climb.update(name: params[:name], category: params[:category], grade: params[:grade], location: params[:location], status: params[:status], notes: params[:notes])
-     @climb.save
-     redirect "/climbs/#{@climb.id}"
-   end
- end
+  patch '/climbs/:id' do
+    @climb = Climb.find_by(params[:id])
+    if params[:name].empty? || params[:location].empty? || params[:status].empty?
+      redirect "/climbs/#{@climb.id}/edit"
+    else
+      @climb.update(name: params[:name], category: params[:category], grade: params[:grade], location: params[:location], status: params[:status], notes: params[:notes])
+      @climb.save
+      redirect "/climbs/#{@climb.id}"
+    end
+  end
  
- post '/climbs/:id' do
+  post '/climbs/:id' do
     @climb = Climb.find_by_id(params[:id])
     if params[:name].empty? || params[:location].empty? || params[:status].empty?
       redirect "/climbs/#{@climb.id}/edit"
