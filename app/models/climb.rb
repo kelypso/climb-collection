21941108a76bf3c4a13644dd5a5b3b69b5df68db
Kelsey Low <kklowlow@gmail.com>
2019-05-18 00:50:48 +0000

    add slug to climb and user models

--- app/models/climb.rb
+++ app/models/climb.rb
@@ -1,8 +1,16 @@
 class Climb < ActiveRecord::Base
-  include Slugifiable::InstanceMethods
-  extend Slugifiable::ClassMethods
-  
   belongs_to :user
   
-  validates :name, uniqueness: true
+  validates :name, presence: true
+  validates :location, presence: true
+  validates :status, presence: true
+  
+  def slug
+     name.downcase.split.join("-")
+   end
+
+   def self.find_by_slug(slug)
+     self.all.detect {|s| s.slug == slug}
+   end
+   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,17 @@
 class User < ActiveRecord::Base
-  include Slugifiable::InstanceMethods
-  extend Slugifiable::ClassMethods
-  
   has_many :climbs
   
-  validates :name, uniqueness: true
-  validates :email, uniqueness: true
+  validates :username, uniqueness: { case_sensitive: false }
+  validates :email, uniqueness: {case_sensitive: false }
+  validates :username, :email, :password, presence: true
+
+  def slug
+    username.downcase.split.join("-")
+  end
+
+  def self.find_by_slug(slug)
+    self.all.detect {|s| s.slug == slug}
+  end
   
   has_secure_password
 end
